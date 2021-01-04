f6500b0270a1f8ed05cae8b63cb7027b141e0484
Gahee <jghgahee12@gmail.com>
2021-01-04 20:07:58 +0900

    make component

--- src/App.js
+++ src/App.js
@@ -44,8 +44,21 @@ function App() {
         <hr/>
       </div>
 
+      <Modal />
+
     </div>
   );
+}
+
+// component만들기
+function Modal(){
+  return (
+    <div className = "modal"> 
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
 }
 
 export default App;
