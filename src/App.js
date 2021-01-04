672358206bb2bf6c07eb6faa81ad63a45ac04d5c
Gahee <jghgahee12@gmail.com>
2021-01-04 19:44:12 +0900

    ㄷㅍ두

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import './App.css';
 function App() {
 
   let style1 = {color:'pink', fontSize: '18px'}
-  let posts = '개발 블로그';
 
   // state 사용
-  let [title, title_change] = useState(['나의 개발 블로그','나의 일기장','study plan','joying']);
+  let [title, title_change] = useState(['오늘 뭐하지?','diary','study plan']);
   let [date, date_change] = useState(['1월 1일 발행','1월 2일 발행','1월 3일 발행','1월 4일 발행']);
+  let [good, good_change] = useState(0);
+
+  function retitle(){
+    var newArr = [...title];
+    newArr[0] = '심심해';
+    title_change( newArr );
+  }
 
   return (
     <div className="App">
@@ -18,8 +24,10 @@ function App() {
         <div style = {style1}>개발 Blog</div>
       </div>
 
+      <button onClick = { retitle }> 버튼 </button>
+
       <div className = "list">
-        <h3> { title[0] } </h3>
+        <h3> { title[0] } <span onClick = { () => { good_change(good + 1 )}}>👍</span> {good} </h3>
         <p> { date[0] } </p>
         <hr/>   
       </div>
@@ -33,12 +41,6 @@ function App() {
       <div className = "list">
         <h3> { title[2] }</h3>
         <p> { date[2] }</p>
-        <hr/>
-      </div>
-
-      <div className = "list">
-        <h3> { title[3] }</h3>
-        <p> { date[3] }</p>
         <hr/>
       </div>
 
